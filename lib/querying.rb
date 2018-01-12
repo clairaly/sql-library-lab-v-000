b330b4b1f49bda56dcf1bb3bab32a7a77d3cec9c
Alyssa Clair <clairaly13@gmail.com>
2018-01-12 21:16:50 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,11 +1,12 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM series
+  WHERE series_id = 1
+  ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
   "Write your SQL query here"
 end
-
 
 def select_value_and_count_of_most_prolific_species
   "Write your SQL query here"
