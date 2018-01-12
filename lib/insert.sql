a4360d752c643f0d100d16a365d001d3144c19db
Alyssa Clair <clairaly13@gmail.com>
2018-01-12 20:08:11 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,28 +1,10 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (value1, value2, value3,....)
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Lord of the Rings", 1, 1), (2, "Second Series", 2, 2)
 
 INSERT INTO subgenres (id, name) VALUES (1, "fantasy"), (2, "comedies of manners");
 
 INSERT INTO authors (id, name) VALUES (1, "J. R. R. Tolkien"), (2, "Jane Austen");
 
-INSERT INTO books (id, title, year, series_id) VALUES
-
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
-
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
-
-INSERT INTO character_books (id, book_id, character_id) VALUES
-
-INSERT INTO character_books (id, book_id, character_id) VALUES
-
-
-
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
-
-INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
-
-INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
-
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "The Hobbit", 1937, 1), (2, "The Fellowship of the Ring", 1954, 1), (3, "The Two Towers", 1954, 1), (4, "The Return of the King", 1955, 1), (5, "Pride and Prejudice", 1813, 2), (6, "Emma", 1815, 2);
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
@@ -32,9 +14,3 @@ INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1,
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
 
-
-or
-
-INSERT INTO table_name
-(column1, column2, column3,...)
-VALUES (value1, value2, value3,....)
