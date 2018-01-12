1c7026c869ce9f53cd24dd32671e1ec75499c8b0
Alyssa Clair <clairaly13@gmail.com>
2018-01-12 21:19:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,10 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM species
+  GROUP by species
+  ORDER by COUNT(species) desc
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
