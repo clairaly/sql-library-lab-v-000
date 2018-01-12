2b9c63abfd415d4593b9227f5645691bf8170df8
Alyssa Clair <clairaly13@gmail.com>
2018-01-12 21:22:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,12 +17,23 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors
+  INNER JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM series
+  INNER JOIN characters
+  ON series.id = characters.series_id
+  GROUP BY characters.series_id
+  ORDER BY COUNT(characters.species = 'human')
+  DESC
+  LIMIT 1;"
 end
+
 
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
