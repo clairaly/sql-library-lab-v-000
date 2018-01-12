a402c7cf4609c9f471bcab7abdd2b8f71f9ee24b
Alyssa Clair <clairaly13@gmail.com>
2018-01-12 19:40:32 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,17 +1,26 @@
-CREATE TABLE table_name
-(
-column_name1 data_type,
-column_name2 data_type,
-column_name3 data_type,
-...
-)
-
 CREATE TABLE series(
   id INTEGER PRIMARY KEY,
-  title TEXT, 
-  author TEXT, 
-  sub-genre_id INTEGER,
+  title TEXT,
+  author TEXT,
+  sub_genre_id INTEGER,
   author_id INTEGER
+);
+
+CREATE TABLE sub_genre(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
 );
 
 CREATE TABLE characters(
@@ -23,3 +32,8 @@ CREATE TABLE characters(
   series_id INTEGER
 );
 
+CREATE TABLE character_books(
+  id INTEGER PRIMARY KEY,
+  book_id INTEGER,
+  character_id INTEGER
+);
