f94e4198151eb4439439d356f23a1fd052c8cc65
Alyssa Clair <clairaly13@gmail.com>
2018-01-12 21:12:33 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,7 +8,7 @@ INSERT INTO books (id, title, year, series_id) VALUES (1, "The Hobbit", 1937, 1)
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Frodo Baggins", "Sam!", "hobbit", 1, 1), (2, "Samwise Gamgee", "Frodo!", "hobbit", 1, 1), (3, "Gandalf", "Thou shall not pass!", "wizard", 1, 1), (4, "Aragorn", "If by my life or death I can protect you, I will.", "human", 1, 1);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Elizabeth Bennet", "I hope I never ridicule what is wise or good. Follies and nonsense, whims and inconsistencies, do divert me, I own, and I laugh at them whenever I can.", "human", 2, 2), (6, "Mr. Darcy", "My good opinion once lost is lost for ever.", "human", 2, 2), (7, "Emma", "Men of sense, whatever you may choose to say, do not want silly wives.", "human", 2, 2), (8, "Mr. Knightley", "I cannot make speeches, Emma . . . If I loved you less, I might be able to talk about it more.", "human", 2, 2);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Elizabeth Bennet", "I hope I never ridicule what is wise or good. Follies and nonsense, whims and inconsistencies, do divert me, I own, and I laugh at them whenever I can.", "human", 2, 2), (6, "Mr. Darcy", "My good opinion once lost is lost for ever.", "human", 2, 2), (7, "Emma", "Men of sense, whatever you may choose to say, do not want silly wives.", "human", 2, 2), (8, "Mr. Knightley", "I cannot make speeches, Emma... If I loved you less, I might be able to talk about it more.", "human", 2, 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
